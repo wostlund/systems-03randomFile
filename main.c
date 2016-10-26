01fac3e9ab02722c89845731eb40dfbfc3b80b8f
wostlund <wostlund@stuy.edu>
2016-10-26 16:42:31 -0400

    Made more improvement

--- main.c
+++ main.c
@@ -8,75 +8,42 @@
 #include <string.h>
 
 int* genArray(){
-  srand(time(0));
+  umask(0000);
+  int* array = calloc(10, sizeof(int));
   int i;
-  int* k = calloc(110, sizeof(char));
-  printf("Generating Random Numbers:\n");
-  srand(time(0));
-  for(i=0; i<10; i++){
-    int j = rand();
-    printf("     random %d: %d\n", i, j);
-    *(k+i) = j;
+  printf("Reading file...\n\n");
+  for(i = 0; i < 10; i++){
+    int y = open("file.txt", O_RDONLY, 0444);
+    int j;
+    read(y, &j, sizeof(int));
+    *(array + i) = j;
   }
-  return k;
+  return array;
 }
 
-char* setString(int* x){
-  int a = 0;
-  int i;
-  char* y = calloc(111, sizeof(char));
-  for(i=0; i<10; i++){
-    char* k = calloc(1, sizeof(int*));
-    sprintf(k, "%d", x[i]);
-    strcat(k, "\n");
-    strcat(y, k);
+void printArray(int* array, char size){
+  char i;
+  for(i = 0; i < size; i++){
+    printf("     array[%d] = %d\n", i, array[i]);
   }
-  //printf("\n%s\n", y);
-  return y;
-}
-
-int* changeToInt(char* x){
-  int i;
-  int* ans = calloc(10, sizeof(int));
-  int j;
-  for(i = 0; i < strlen(x); i++){
-    char* a = calloc(10, sizeof(char));
-    int l = 0;
-    while(*(x+i)!='\n'){
-      *(a+l) = *(x+i);
-      i++;
-      l++;
-    }
-    *(ans+j) = atoi(a);
-    printf("%d\n ", *(ans+j));
-    free(a);
-  }
-  free(x);
-  return ans;
 }
 
 int main(){
-
   umask(0000);
+  srand(time(NULL));
+  int* array1 = calloc(40, 1);
   char i;
-  char* k = setString(genArray());
-  int x = open("file1.txt", O_CREAT | O_RDWR, 0666);
-
-  printf("\nWriting Numbers to File...\n\n");
-  write(x, k, 111);
-  //free(k);
-
-  char* y = calloc(111, sizeof(char));
-  read(x, y, 111);
-
-  printf("%s\n", y);
-  printf("Reading Numbers to File...\n\n");
-  printf("Veritfication that written values were the same:\n");
-
-  int* array2 = calloc(10, sizeof(int));
-  array2 = changeToInt(k);
+  int file = open("file.txt", O_RDWR | O_CREAT, 0666);
+  printf("Generating random numbers...\n\n");
   for(i = 0; i < 10; i++){
-    printf("    random %d: %d\n", i, *(array2+i));
+    *(array1 + i) = rand();
+    printf("    random %d: %d\n", i, array1[i]);
   }
+  printf("\nWriting numbers into file\n\n");
+  for(i = 0; i < 10; i++){
+    write(file, array1 + i, sizeof(int));
+  }
+  int* array2 = genArray();
+  printArray(array2, 10);
   return 0;
 }
